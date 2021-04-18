bb27083dcd85653696eb6221f77bfb11070a0623
Roman <grevzi@gmail.com>
2021-04-18 13:02:44 +0300

    layout file  - finished

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,6 +2,11 @@ import Page from "../components/Page";
 import Router from 'next/router'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
+import '../components/styles/nprogress.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const App = ({Component, pageProps}) => {
     return (
